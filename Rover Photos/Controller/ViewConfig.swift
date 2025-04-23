f337ccd1f9ea2deb6dfabb80b829aa761a066e40
Randy <randyvarela5@gmail.com>
2025-04-23 18:36:32 -0500

    moved datepickerConfig func to its own file

--- Rover Photos/Controller/ViewConfig.swift
+++ Rover Photos/Controller/ViewConfig.swift
@@ -56,37 +56,4 @@ extension ViewController {
         roverImage.layer.borderColor = UIColor.white.cgColor
         roverImage.layer.cornerRadius = 10.0
     }
-    
-    func updateDatePickerRange() {
-        print("Date picker range updated")
-        print("Current button title is: \(roverPopUpBtn.currentTitle ?? "No title")")
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        if roverPopUpBtn.currentTitle == "Curiosity" {
-            if let minDate = dateFormatter.date(from: "2012-08-06") {
-                datePicker.minimumDate = minDate
-            }
-            if let maxDate = dateFormatter.date(from: Date().description) {
-                datePicker.maximumDate = maxDate
-            }
-        }
-        if roverPopUpBtn.currentTitle == "Opportunity" {
-            if let minDate = dateFormatter.date(from: "2004-01-25") {
-                datePicker.minimumDate = minDate
-            }
-            if let maxDate = dateFormatter.date(from: "2018-05-31") {
-                datePicker.maximumDate = maxDate
-            }
-        }
-        if roverPopUpBtn.currentTitle == "Spirit" {
-            if let minDate = dateFormatter.date(from: "2004-01-04") {
-                datePicker.minimumDate = minDate
-            }
-            if let maxDate = dateFormatter.date(from: "2010-02-28") {
-                datePicker.maximumDate = maxDate
-            }
-        }
-    }
 }
